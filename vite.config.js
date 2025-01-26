da34b274d3c80242e2135b1d839359835df10484
CyrusKell <cyfeye@gmail.com>
2025-01-26 06:11:17 -0500

    trying to fix prod still

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  optimizeDeps: {
-    include: ['@tensorflow/tfjs'],
-  },
-  build: {
-    commonjsOptions: {
-      include: ["@tensorflow/tfjs", /node_modules/],
-    },
-  },
-  plugins: [
-  ],
+  
 })
