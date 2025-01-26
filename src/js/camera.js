2430184fc0e9f2ac19c5bb21f41972011757a7e5
Nora Choukri <norachoukri16@gmail.com>
2025-01-25 23:16:40 -0500

    testing mobile pt 3

--- src/js/camera.js
+++ src/js/camera.js
@@ -125,10 +125,12 @@ document.getElementById("video-stream-checkbox").addEventListener("click", () =>
         const rawBase64Data = data.split(",")[1]; // Extract the Base64 portion
    
         try {
+          responsiveVoice.clickEvent();
+          responsiveVoice.speak(".", "UK English Female");
           const response = await respond(rawBase64Data); // Call respond with Base64 image
           console.log("AI Response:", response); // Log AI response
           // Speak the response out loud
-          responsiveVoice.clickEvent();
+
           responsiveVoice.speak(response, "UK English Female", {rate: 1.1});
         } catch (error) {
           console.error("Error in respond function:", error);
